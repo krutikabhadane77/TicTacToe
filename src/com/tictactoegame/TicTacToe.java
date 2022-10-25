b7098a95cd46a17ee27f42efa5efa91b37323381
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-25 23:02:30 +0530

    Added: UC5 Check the free space before making desired move

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -9,11 +9,11 @@ public class TicTacToe {
         return board;
     }
 
-    public static char chooseLetter(char givenInput) {
-       return Character.toUpperCase(givenInput);
+    public static char getInput(char givenInput) {
+        return Character.toUpperCase(givenInput);
     }
 
-    public static void showBoard(char[] board) {
+    public static void showBoard(char[] board){
         System.out.println(board[1] + " | " + board[2] + " |" + " " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " |" + " " + board[6]);
@@ -21,29 +21,41 @@ public class TicTacToe {
         System.out.println(board[7] + " | " + board[8] + " |" + " " + board[9]);
     }
 
-    public static boolean checkPosition(char[] board, Scanner input) {
-        System.out.println("Enter the user input ");
-        int userInput = input.nextInt();
-        boolean flag = true;
-        while (flag) {
-            if (board[userInput] == ' ') {
-                return flag;
+    public static boolean isBoardEmpty(char[] board, int index) {
+        return board[index] == ' ';
+    }
+
+    public static int getUserMove(char[] board, Scanner getInput) {
+        while (true) {
+            System.out.println("Enter the index you want to move");
+            int index = getInput.nextInt();
+            if ((index > 0 && index < 10) && isBoardEmpty(board, index)) {
+                return index;
+            } else if ((index <= 0 && index >= 10)) {
+                System.out.println("Invalid Index Try Again!!!");
+            } else {
+                System.out.println("Already Occupied");
             }
         }
-        return false;
+    }
+
+    // make move to the given index
+    public static void makeMove(char[] board, int index, char letterInput) {
+        if (isBoardEmpty(board, index)) {
+            board[index] = letterInput;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program");
-        Scanner input = new Scanner(System.in);
-        char givenInput = input.next().charAt(0);
+        Scanner userInput = new Scanner(System.in);
+        char givenInput = userInput.next().charAt(0);
         char[] board = createBoard();
-        char ch = chooseLetter((chooseLetter(givenInput) == 'X' ? 'O' : 'X'));
+        char getLetterInput = getInput((givenInput) == 'X' ? 'O' : 'X');
         showBoard(board);
-        boolean isEmpty = checkPosition(board, input);
-        if (isEmpty) {
-            showBoard(board);
-        }
+        int userMove = getUserMove(board, userInput);
+        showBoard(board);
+        makeMove(board, userMove, getLetterInput);
+       showBoard(board);
     }
 }
-
