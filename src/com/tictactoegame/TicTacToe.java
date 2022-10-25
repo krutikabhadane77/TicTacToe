4b4ff002ae96ab22afdd6fa081014f8ad2026472
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-25 22:16:17 +0530

    Added: UC3 Display the current board

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -1,28 +1,41 @@
 package com.tictactoegame;
 import java.util.Scanner;
 public class TicTacToe {
-    public static void createBoard() {
+    public static char[] createBoard() {
         char[] board = new char[10];
         for (int position = 1; position < 10; position++) {
             board[position] = ' ';
         }
+        return board;
     }
 
-    public static char chooseLetter() {
+    public static char chooseLetter(Scanner input) {
         System.out.println("Choose your letter 'X' or 'O'");
-        Scanner input = new Scanner(System.in);
         char givenInput = input.next().charAt(0);
-        if (givenInput == 'X' || givenInput == 'x') {
-            return givenInput;
-        } else if (givenInput == 'O' || givenInput == 'o') {
+        if (givenInput == 'X' || givenInput == 'x')
+        {
             return givenInput;
         }
-        return 'N';
+        else if (givenInput == 'O' || givenInput == 'o')
+        {
+            return givenInput;
+        }
+        return givenInput;
+    }
+
+    public static void showBoard(char[] board) {
+        System.out.println(board[1]+" | "+board[2]+" |"+" "+board[3]);
+        System.out.println("---------");
+        System.out.println(board[4]+" | "+board[5]+" |"+" "+board[6]);
+        System.out.println("---------");
+        System.out.println(board[7]+" | "+board[8]+" |"+" "+board[9]);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program");
-        createBoard();
-        chooseLetter();
+        Scanner input = new Scanner(System.in);
+        char[] board = createBoard();
+        char chooseValue = chooseLetter(input);
+        showBoard(board);
     }
 }
