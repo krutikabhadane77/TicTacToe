c3f183f8544958673fbe95d1a0dffc8f87681491
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-25 22:07:17 +0530

    Added: UC2 Player choose a letter X or O

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -1,19 +1,28 @@
 package com.tictactoegame;
-
+import java.util.Scanner;
 public class TicTacToe {
-    public void createboard(int size) {
-        char[] board = new char[size];
-        for(int i = 1; i < 10; i++) {
-            board[i] = ' ';
+    public static void createBoard() {
+        char[] board = new char[10];
+        for (int position = 1; position < 10; position++) {
+            board[position] = ' ';
         }
+    }
 
+    public static char chooseLetter() {
+        System.out.println("Choose your letter 'X' or 'O'");
+        Scanner input = new Scanner(System.in);
+        char givenInput = input.next().charAt(0);
+        if (givenInput == 'X' || givenInput == 'x') {
+            return givenInput;
+        } else if (givenInput == 'O' || givenInput == 'o') {
+            return givenInput;
+        }
+        return 'N';
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program");
-        TicTacToe tg = new TicTacToe();
-        tg.createboard(10);
-
+        createBoard();
+        chooseLetter();
     }
-
 }
